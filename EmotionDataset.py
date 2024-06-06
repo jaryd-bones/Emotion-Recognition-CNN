e561b584df0922771d8d0d313e4b3e381a20d5e1
jbones02 <jaryd02@gmail.com>
2024-06-05 18:00:08 -0700

    got basic model working

--- EmotionDataset.py
+++ EmotionDataset.py
@@ -1,8 +1,7 @@
-import os
-import torch
-from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
+import torch
+from torch.utils.data import Dataset
 
 class EmotionDataset(Dataset):
     def __init__(self, dataframe, transform=None):
@@ -14,10 +13,16 @@ class EmotionDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.dataframe.iloc[idx, 0]
-        image = Image.open(img_path).convert('RGB')
-        label = self.dataframe.iloc[idx, 1]
+        label = self.dataframe.iloc[idx, 1:].values.astype('float32')
 
+        image = Image.open(img_path).convert('RGB')
         if self.transform:
             image = self.transform(image)
 
-        return image, label
+        return image, torch.from_numpy(label)
+
+def get_transform():
+    return transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+    ])
