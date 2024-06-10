ce273db741818f9e4d16cffa53616fd4a233e77b
jbones02 <jaryd02@gmail.com>
2024-06-10 16:28:52 -0700

    trained final model

--- EmotionCNN.py
+++ EmotionCNN.py
@@ -5,7 +5,6 @@ class EmotionCNN(nn.Module):
     def __init__(self):
         super(EmotionCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.fc1 = nn.Linear(64 * 12 * 12, 512)
